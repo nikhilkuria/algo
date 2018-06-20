6aa89a4c45e862bfc01bb10be855c08418f7e3d7
nikhilkuria <nikhilkuria@gmail.com>
2018-06-20 10:01:03 +0200

    improving performance, but not enough

--- src/hackerrank/algo/implementation/climbing_leaderboard.py
+++ src/hackerrank/algo/implementation/climbing_leaderboard.py
@@ -3,17 +3,20 @@ import os
 import random
 import re
 import sys
+import bisect
 
 
 # Complete the climbingLeaderboard function below.
 def climbingLeaderboard(scores, alice):
     positions = []
     unique_scores = set(scores)
-    sorted_scores = sorted(unique_scores, reverse = True)
-    for alice_score in alice:
-        if sorted_scores.index(alice_score)
-        unique_scores.add(alice_score)
-        positions.append(sorted_scores.index(alice_score) + 1)
+    sorted_scores = sorted(unique_scores)
+    alice_scores = sorted(alice)
+
+    for alice_score in alice_scores:
+        if alice_score not in sorted_scores:
+            bisect.insort(sorted_scores, alice_score)
+        positions.append(len(sorted_scores) - sorted_scores.index(alice_score))
 
     return positions
 
